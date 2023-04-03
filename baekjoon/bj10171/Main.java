d9da2c402303bc09e8d5586d648091bfcbd308d3
junggeun <junggeun@hanchang.co.kr>
2023-04-03 10:27:19 +0900

    solve: #10 백준 B5 개 (10172)

--- baekjoon/bj10171/Main.java
+++ baekjoon/bj10171/Main.java
@@ -3,15 +3,13 @@ package baekjoon.bj10171;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+
 
     }
 }
---
+++ baekjoon/bj10172/README.md
@@ -0,0 +1,1 @@
+![image](https://user-images.githubusercontent.com/96937623/229389401-9b11b187-6a93-4423-9ac3-1f82555045ad.png)
