750b12b1f78c29da5975424249c5f0baf7127a5c
junggeun <junggeun@hanchang.co.kr>
2023-04-28 14:24:15 +0900

    solve: #33 백준 B3 최댓값 (2562)

---
+++ baekjoon/bj2562/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.bj2562;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;
+        int index = 0;
+
+        for(int i = 0; i < 9; i++){
+
+            int value = Integer.parseInt(br.readLine());
+
+            if(value > max){
+                max = value;
+                index = i + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
---
+++ baekjoon/bj2562/README.md
@@ -0,0 +1,1 @@
+![image](https://user-images.githubusercontent.com/96937623/235031429-8ba23da5-cc54-45c5-8f65-9f9cf9131624.png)
