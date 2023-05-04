783c95aeaa608fd386da5f70b89e6032a89cbfb4
junggeun <junggeun@hanchang.co.kr>
2023-05-04 11:03:59 +0900

    solve: #36 백준 B5 과제 안 내신 분..? (5597)

---
+++ baekjoon/bj5597/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.bj5597;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] arr = new boolean[31];
+
+        for(int i = 0; i < 28; i++){
+            int n = Integer.parseInt(br.readLine());
+            arr[n] = true;
+        }
+        for(int i = 1; i <=30; i++){
+            if(!arr[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ baekjoon/bj5597/README.md
@@ -0,0 +1,2 @@
+![image](https://user-images.githubusercontent.com/96937623/236082712-082cd789-a9f8-4a64-946c-691be0873f65.png)
+![image](https://user-images.githubusercontent.com/96937623/236082764-1a159308-cf4f-4ffa-971c-c837c969eba2.png)
