c446f347b7953e4f5d340395c30d6f1c9081e7ac
junggeun <junggeun@hanchang.co.kr>
2023-05-15 09:30:15 +0900

    solve: #43 백준 B5 아스키 코드 (11654)

---
+++ baekjoon/bj11654/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.bj11654;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+        int ch = in.next().charAt(0);
+        System.out.println(ch);
+
+        //int a = System.in.read();
+        //System.out.println(a);
+
+    }
+}
---
+++ baekjoon/bj11654/README.md
@@ -0,0 +1,1 @@
+![image](https://github.com/geuning/Algorithm/assets/96937623/047f050c-1abc-4fe1-a63e-443ce991f170)
